21b450e5d56c91311178321c7dce00fd15abf32e
rockrick1 <henriquecerquinho@gmail.com>
2023-04-29 12:50:40 -0300

    feat: add files

---
+++ Assets/Scripts/CameraOrbitController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CameraOrbitController : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed;
+
+    void Update ()
+    {
+        if (Input.GetMouseButton(0))
+            RotateCamera();
+    }
+
+    void RotateCamera ()
+    {
+        float xAxis = Input.GetAxis("Mouse X");
+        float yAxis = Input.GetAxis("Mouse Y");
+        
+        if (xAxis != 0 || yAxis != 0)
+        {
+            float xInput = xAxis * rotationSpeed * Time.deltaTime;
+            float yInput = yAxis * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, xInput, Space.World);
+            transform.Rotate(-Vector3.right, yInput);
+        }
+    }
+}
