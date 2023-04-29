dd468aab7c92fbd9b0680973ab250738e6594b47
rockrick1 <henriquecerquinho@gmail.com>
2023-04-29 12:56:38 -0300

    feat: orbit camera clamping

--- Assets/Scripts/CameraOrbitController.cs
+++ Assets/Scripts/CameraOrbitController.cs
@@ -4,23 +4,26 @@ public class CameraOrbitController : MonoBehaviour
 {
     [SerializeField] float rotationSpeed;
 
+    Vector3 localRotation = Vector3.zero;
+
     void Update ()
     {
         if (Input.GetMouseButton(0))
             RotateCamera();
     }
 
+    void SetFocalPoint (Transform point) => transform.position = point.position;
+
     void RotateCamera ()
     {
         float xAxis = Input.GetAxis("Mouse X");
         float yAxis = Input.GetAxis("Mouse Y");
-        
-        if (xAxis != 0 || yAxis != 0)
-        {
-            float xInput = xAxis * rotationSpeed * Time.deltaTime;
-            float yInput = yAxis * rotationSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.up, xInput, Space.World);
-            transform.Rotate(-Vector3.right, yInput);
-        }
+        float xInput = xAxis * rotationSpeed * Time.deltaTime;
+        float yInput = yAxis * rotationSpeed * Time.deltaTime;
+        localRotation.x += xInput;
+        localRotation.y -= yInput;
+        localRotation.y = Mathf.Clamp(localRotation.y, 0f, 85f);
+
+        transform.rotation = Quaternion.Euler(localRotation.y, localRotation.x, 0f);
     }
 }
